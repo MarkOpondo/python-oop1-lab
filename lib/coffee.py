57177f59bf89d01fc6592fce25cd545c00d72401
Mark Opondo <markopondoi.t@gmail.com>
2026-06-13 12:06:33 +0300

    corrections

--- lib/coffee.py
+++ lib/coffee.py
@@ -5,16 +5,18 @@ class Coffee:
         self.size = size
         self.price = price
 
+    @property
     def get_size(self):
         return self._size
     
+    @size.setter
     def set_size(self, value):
         options = ["Small", "Medium", "Large"]
-        for o in options:
-            if value != o:
-                print("Size must be small medium or large")
-            else:
-                self._size = value
+        # for o in options:
+        if value not in options:
+            print("size must be Small, medium, or Large")
+        else:
+            self._size = value
 
     def tip(self, amount):
         self.price += amount
